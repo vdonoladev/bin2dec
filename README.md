c2b2acc2fea3439a9d8a894499ebb3fa7502981b
vdonoladev <victordonolaf@gmail.com>
2020-07-17 15:37:17 -0300

    Pequenas alterações em todos os arquivos (exceto css)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bin2dec
-O Bin2Dec permite que o usuário insira seqüências de até 8 dígitos binários, 0 e 1, em qualquer sequência e, em seguida, exibe seu equivalente decimal.
+O Bin2Dec permite que o usuário insira sequências de dígitos binários, 0 e 1, em qualquer sequência e, 
+em seguida, exibe seu equivalente decimal.
+
--- main.js
+++ main.js
@@ -4,9 +4,9 @@
 // eslint-disable-next-line no-unused-vars
 function convert() {
     const binary = document.getElementById('b-input').value;
-    if (binary === '') return alert('Please, enter a binary number');
+    if (binary === '') return alert('Por favor, insira um número binário');
     binary.split('').map((char) => {
-        if (char !== '0' && char !== '1') return alert('Please, enter a binary number');
+        if (char !== '0' && char !== '1') return alert('Por favor, insira um número binário');
     });
     const decimal = parseInt(binary, 2);
     document.getElementById('d-input').value = decimal;
